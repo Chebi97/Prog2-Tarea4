057bf32ec51df7585ff1ac3888c51e71b86f2b78
Chebi97 <chebi97@gmail.com>
2016-04-19 19:25:14 -0300

    Hechas funciones hasta imprimir_binario
    Solo falta filtrado_binario

--- tarea3/cpp/binario.cpp
+++ tarea3/cpp/binario.cpp
@@ -85,7 +85,7 @@ binario crear_filtrado(const int clave, comp_t criterio, const binario b) {
               return fizquierdo;
             } else {
               buscador = b->izq; //no hay un mayor para arboles en este programa de m***?
-                while (buscador != NULL){
+                AZ (buscador != NULL){
                   buscador = buscador->der;
                 }
               mayor = buscador->dato;
@@ -266,19 +266,85 @@ texto_t camino_a_texto(const camino_t c) {
   return res;
 }
 
+static camino_t buscar_camino_aux(const lista l, const binario b, camino_t c);
+
+camino_t buscar_camino_aux(const lista l, const binario b, camino_t c) {
+
+    if (!es_vacia_lista(l) && !es_vacio_binario(b)) {
+      if (texto_info(info_lista(inicio_lista(l), l)) == texto_info(b->dato)) {
+        lista lTail = segmento_lista(siguiente(inicio_lista(l), l), final_lista(l), l);
+        if (!es_vacia_lista(lTail)) {
+          if (texto_info(info_lista(inicio_lista(lTail),l)) > texto_info(b->dato)) {
+            c.ramas[c.cantidad_ramas] = der;
+            c.cantidad_ramas++;
+            buscar_camino_aux(lTail, b->der, c);
+          } else {
+            c.ramas[c.cantidad_ramas] = izq;
+            c.cantidad_ramas++;
+            buscar_camino_aux(lTail, b->izq, c);
+          }
+        }
+      }
+    } else if (!es_vacia_lista(l) && !es_vacio_binario(b)) {
+      c.existe = true;
+    }
+    return c;
+  }
+
 camino_t buscar_camino(const lista l, const binario b) {
   camino_t res;
   res.existe = false;
   res.cantidad_ramas = 0;
 
-  if (!es_vacia_lista(l) && !es_vacio_binario(b)) {
-    if (texto_info(info_lista(inicio_lista(l), l)) == texto_info(b->dato)) {
-      lista lTail = segmento_lista(siguiente(inicio_lista(l), l), final_lista(l), l);
-      //no esta terminada
-    }
-  }
+  res = buscar_camino_aux(l, b, res);
+
+  return res;
 }
 
-/*
+static void imprimir_binario_aux(const binario b, nat a);
 
-void imprimir_binario(const binario b) {}*/
+void imprimir_binario_aux(const binario b, nat a) {
+  if (!es_vacio_binario(b)) {
+      if (b->izq == NULL && b->der == NULL) {
+        for (nat i = 0; i < a; i++)
+          printf("%c", "-");
+        escribir_texto(info_a_texto(b->dato));
+        printf("\n");
+        a--;
+      } else if (b->der == NULL) {
+        for (nat i = 0; i < a; i++)
+          printf("%c", "-");
+        escribir_texto(info_a_texto(b->dato));
+        printf("\n");
+        a++;
+        imprimir_binario(b->izq);
+      } else if (b->izq == NULL) {
+        a++;
+        imprimir_binario(b->der);
+        a--;
+        for (nat i = 0; i < a; i++)
+          printf("%c", "-");
+        escribir_texto(info_a_texto(b->dato));
+        printf("\n");
+      } else {
+        a++;
+        imprimir_binario(b->der);
+        a--;
+        nat i = 0;
+        for (nat i = 0; i < a; i++)
+          printf("%c", "-");
+        escribir_texto(info_a_texto(b->dato));
+        printf("\n");
+        a++;
+        imprimir_binario(b->izq);
+        a--;
+      }
+    }
+}
+
+
+void imprimir_binario(const binario b) {
+  imprimir_binario_aux(b, 0);
+}
+
+
