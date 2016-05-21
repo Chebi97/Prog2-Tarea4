418a6f584db238a9c1fcebfade703440fc1d4360
dsusviela <yothebest009@gmail.com>
2016-05-21 17:39:06 -0300

    Update pila.cpp

--- tarea4/cpp/pila.cpp
+++ tarea4/cpp/pila.cpp
@@ -1,13 +1,12 @@
-
 struct rep_pila {
   nat altura;
   nat altura_maxima;
-  elchebimedira *tope;
+  node *tope;
 };
 
-struct elchebimedira {
+struct node {
   texto_t texto;
-  elchebimedira *siguiente;
+  node *siguiente;
 };
 
 
@@ -29,7 +28,7 @@ pila crear_pila(const nat tamanio){
  */
 void apilar(const texto_t t, pila &p){
     if (!es_llena_pila(p)){
-        elchebimedira *res = new elchebimedira;
+        node *res = new node;
         res->texto = t;
         res->siguiente = p->tope;
         p->tope = res;
@@ -43,28 +42,44 @@ void apilar(const texto_t t, pila &p){
   Si es_vacia_pila(p) no hace nada.
  */
 void desapilar(pila &p){
-    
+    node* aux = p->tope;
+    delete p->tope;
+    p->tope->siguiente = aux;
 }
 
 /*
   Devuelve `true` si `p` es vacía (no tiene elementos), o `false` en otro caso.
  */
-bool es_vacia_pila(const pila p){}
+bool es_vacia_pila(const pila p){
+  bool res = p->tope == NULL;
+  return res;
+}
 
 /*
   Devuelve `true` si la cantidad de elementos en `p` es `tamanio` (siendo
   `tamanio` el valor del parámetro con que fue creada `p`), o `false` en otro
   caso.
  */
-bool es_llena_pila(const pila p){}
+bool es_llena_pila(const pila p){
+  bool res = p->altura_maxima == p->altura;
+  retunr res;
+}
 
 /*
   Devuelve el elemento que está en la cima de `p`.
   Precondición: ! es_vacia_pila(p){}
  */
-texto_t cima(const pila p){}
+texto_t cima(const pila p){
+  return p->tope;
+}
 
 /*
   Libera la memoria asignada a `p` y la de todos sus elementos.
  */
-void liberar_pila(pila &p){}
+void liberar_pila(pila &p){
+  while (!es_vacia_pila){
+    texto_t aux = p->tope->texto;
+    desapilar(p);
+    liberar_texto(aux);
+  }
+}
